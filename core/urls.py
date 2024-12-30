87ebf16f393a0605cc7a6610b170785e1b1b3f1f
ashim-nepal <npashim015@gmail.com>
2024-12-30 09:27:52 +0545

    Completed the frontend of Home page and profile pages

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import home
-
+from . import views
 urlpatterns = [
     path('', home, name='home'),
+    path('docProfile/', views.docProfile),
+    path('patientProfile/', views.patientProfile),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def docProfile(request):
+    return render(request, "docProfile.html")
+
+def patientProfile(request):
+    return render(request,"patientProfile.html")
